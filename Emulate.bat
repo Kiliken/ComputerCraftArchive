5b1434b2c08ba7b068214656c40574639d548aff
Kiliken <francescopaolomariani7@gmail.com>
2025-07-24 17:22:57 +0900

    Hello World in lua

--- Emulate.bat
+++ Emulate.bat
@@ -1,5 +1,5 @@
 @echo off
 
-robocopy "%cd%/computer" "%appdata%/CraftOS-PC/computer" * /nocopy /purge /E
+robocopy "%cd%/computer" "%appdata%/CraftOS-PC/computer" /MIR
 
 %cd%\CraftOS\CraftOS-PC.exe
---
+++ computer/0/HelloWorld.lua
@@ -0,0 +1,4 @@
+term.clear()
+term.setCursorPos(1,1)
+term.write("Hello World!")
+term.setCursorPos(1,2)
